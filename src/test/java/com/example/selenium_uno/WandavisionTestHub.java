d65982f96eab2cb435b0bf540977143cb6172008
Administrator <admin@example.com>
2021-02-05 14:06:16 +0000

    Update WandavisionTestHub.java

--- src/test/java/com/example/selenium_uno/WandavisionTestHub.java
+++ src/test/java/com/example/selenium_uno/WandavisionTestHub.java
@@ -31,9 +31,10 @@ public class WandavisionTestHub {
   private WebDriver driver;
   private Map<String, Object> vars;
   JavascriptExecutor js;
+  DesiredCapabilities capabilities = new DesiredCapabilities();
   @BeforeEach
   public void setUp() throws MalformedURLException {
-    DesiredCapabilities capabilities = new DesiredCapabilities();
+    
     capabilities.setBrowserName(System.getProperty("browser"));
     Configuration.startMaximized = true;
     open("about:blank");
